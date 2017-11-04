2d4aa8ad968e06899d5824979c72209d5e40f726
haib-bwilk <bwilk@hudsonalpha.org>
2017-11-03 22:30:21 -0500

    added page for converting text to hpo to rank list of genes

--- application.py
+++ application.py
@@ -9,7 +9,9 @@ from flask import render_template
 from flask import Response
 from flask import request
 import pickle
-
+import requests
+from urllib.parse import urlencode, quote_plus
+from pprint import pprint
 from HPOParser import getTermsAndSyns
 
 app = Flask(__name__)
@@ -37,17 +39,22 @@ def search():
     terms = getTermsAndSyns('./HPO_graph_data/hp.obo')
     return render_template('search.html', terms=terms)
 
+@app.route('/text')
+def text():
+    return render_template('textToRank.html')
+
 @app.route('/rank', methods=['POST'])
 def rank():
     '''
     This is intended to be the workhorse function.  POST needs to include a "term" list that should be HPO terms matching values from the layered graph.
     '''
-    #"constant" global data
+    # "constant" global data
     mg, restartProb, hpoWeights, bg = getMultigraphVars()
-    
-    #TODO: make this actually work
+
+    # TODO: make this actually work
     hpoTerms = set([str(x) for x in request.form.getlist('term[]')])
-    #startProbs = {('HPO', h) : hpoWeights[h] for h in hpoTerms}
+    pprint(hpoTerms)
+    # startProbs = {('HPO', h) : hpoWeights[h] for h in hpoTerms}
     startProbs = {}
     usedTerms = set([])
     missingTerms = set([])
@@ -57,28 +64,91 @@ def rank():
             usedTerms.add(h)
         else:
             missingTerms.add(h)
-    
+
     rankTypes = set(['gene'])
     rankedGenes = mg.RWR_rank(startProbs, restartProb, rankTypes, bg)
-    
+
     if request.form['action'] == 'JSON':
         rankings = []
         for w, t, l in rankedGenes:
-            rankings.append({'weight':w, 'nodeType':t, 'label':l})
-        
+            rankings.append({'weight': w, 'nodeType': t, 'label': l})
+
         ret = {'rankings': rankings,
                'usedTerms': list(usedTerms),
                'missingTerms': list(missingTerms)}
-        
+
         return jsonify(ret)
     else:
-        ret = 'Used: '+str(usedTerms)+'<br>'
-        ret += 'CSV-used: '+str(';'.join(sorted(usedTerms)))+'<br>'
-        ret += 'Missing: '+str(missingTerms)+'<br><br>'
+        ret = 'Used: ' + str(usedTerms) + '<br>'
+        ret += 'CSV-used: ' + str(';'.join(sorted(usedTerms))) + '<br>'
+        ret += 'Missing: ' + str(missingTerms) + '<br><br>'
         for i, (w, t, l) in enumerate(rankedGenes[0:20]):
-            ret += ' '.join([str(x) for x in (i, w, t, l)])+'<br>'
+            ret += ' '.join([str(x) for x in (i, w, t, l)]) + '<br>'
+        return ret@app.route('/rank', methods=['POST'])
+
+@app.route('/text/rank', methods=['POST'])
+def textrank():
+    '''
+    Workhorse function for converting text to hpo terms to genes.  POST needs to include a "indications" payload that
+    should be text to be annotated by the NCBO annotator.
+    '''
+
+    indicationsText = request.form['indications']
+    payload = {'apikey': '8b5b7825-538d-40e0-9e9e-5ab9274a9aeb',
+               'text': indicationsText,
+               'ontologies': 'HP',
+               'whole_word_only': 'false'}
+    url = 'http://data.bioontology.org/annotator?' + urlencode(payload, quote_via=quote_plus)
+    resp = requests.get(url)
+    pprint(resp.json())
+
+    # "constant" global data
+    mg, restartProb, hpoWeights, bg = getMultigraphVars()
+
+    # TODO: make this actually work
+    hpoTerms = set([])
+    for annot in resp.json():
+        hpoTerms.add(annot['annotatedClass']['@id'].split('/')[-1].replace('_', ':'))
+        # hpoTerms.add(annot['annotatedClass']['@id'].replace('_', ':'))
+
+    pprint(hpoTerms)
+    # startProbs = {('HPO', h) : hpoWeights[h] for h in hpoTerms}
+    startProbs = {}
+    usedTerms = set([])
+    missingTerms = set([])
+    for h in hpoTerms:
+        if h in hpoWeights:
+            startProbs[('HPO', h)] = hpoWeights[h]
+            usedTerms.add(h)
+        else:
+            missingTerms.add(h)
+
+    rankTypes = set(['gene'])
+    rankedGenes = mg.RWR_rank(startProbs, restartProb, rankTypes, bg)
+
+    if request.form['action'] == 'JSON':
+        rankings = []
+        for w, t, l in rankedGenes:
+            rankings.append({'weight': w, 'nodeType': t, 'label': l})
+
+        ret = {'rankings': rankings,
+               'usedTerms': list(usedTerms),
+               'missingTerms': list(missingTerms)}
+
+        return jsonify(ret)
+    elif request.form['action'] == 'GENES':
+        ret = ''
+        for w, t, l in rankedGenes:
+            ret += l + ';'
         return ret
-    
+    else:
+        ret = 'Used: ' + str(usedTerms) + '<br>'
+        ret += 'CSV-used: ' + str(';'.join(sorted(usedTerms))) + '<br>'
+        ret += 'Missing: ' + str(missingTerms) + '<br><br>'
+        for i, (w, t, l) in enumerate(rankedGenes[0:20]):
+            ret += ' '.join([str(x) for x in (i, w, t, l)]) + '<br>'
+        return ret
+
 def getMultigraphVars():
     '''
     Get any variables that are constant between requests from flask.g.
